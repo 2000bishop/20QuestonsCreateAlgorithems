3c351b445a2e50aedee1c924c5d688cfeb75ee45
townsend.bishop <townsend.bishop@vashonsd.org>
2021-12-06 10:47:44 -0800

    assiment for class bolans

---
+++ src/org/vashonsd/Main.java
@@ -0,0 +1,36 @@
+package org.vashonsd;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        int trysLeft = 20;
+        int secretNum = rand.nextInt(100);
+        int userResponse = 0;
+        while(userResponse != secretNum && trysLeft > 0) {
+            System.out.print("guess number between 1 and 100:  ");
+            userResponse = scan.nextInt();
+            if (userResponse == secretNum) {
+                System.out.println("Hooray! You got it in " + (20-trysLeft) + " guesses!");
+                break;
+            }
+            trysLeft--;
+            if (userResponse > secretNum) {
+                System.out.println("No the ancer is less");
+            }
+            if (userResponse < secretNum) {
+                System.out.println("No the ancer is more");
+            }
+            System.out.println("you have: " + trysLeft + " trys left");
+        }
+        if(trysLeft == 0) {
+            System.out.println("Sorry, looks like you ran out of guesses.");
+        }
+        }
+    }
+
